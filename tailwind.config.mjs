e8197a0c7a068eb9977b7ba3f6ae7890bae40b72
Rose Bowen <rosebowen1997@gmail.com>
2024-07-26 22:44:36 +0100

    navbar styles and animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      primary: {
+        peach: "#FFBFC3",
+        purple: "#5B16FF",
+      },
+    },
+    extend: {
+      transitionTimingFunction: {
+        primary: "cubic-bezier(.16,1,.3,1)",
+      },
+			transitionDuration: {
+				primary: '750ms'
+			}
+    },
+  },
+  plugins: [],
+};
